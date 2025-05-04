5ecbd36d9f7f269ce14d8fcc9fdc1058aed6f05b
priyanshur785 <129311736+priyanshur785@users.noreply.github.com>
2025-05-04 19:42:42 +0530

    logger.py

--- logger.py
+++ logger.py
@@ -1,10 +1,21 @@
+import os
+import json
 import gspread
-from google.oauth2.service_account import Credentials
 from datetime import datetime
+from google.oauth2.service_account import Credentials
 
-def log_to_google_sheet(sheet_name, row_data, worksheet_title="Sheet1", creds_path="config/google_credentials.json"):
+def log_to_google_sheet(sheet_name, row_data, worksheet_title="Sheet1"):
     scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-    creds = Credentials.from_service_account_file(creds_path, scopes=scopes)
+
+    # Load credentials from environment variable
+    creds_json = os.environ.get("GOOGLE_CREDENTIALS_JSON")
+    if not creds_json:
+        print("Google Sheets logging skipped. Missing credentials.")
+        return
+
+    creds_dict = json.loads(creds_json)
+    creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
+
     client = gspread.authorize(creds)
     sheet = client.open(sheet_name).worksheet(worksheet_title)
     sheet.append_row(row_data)
